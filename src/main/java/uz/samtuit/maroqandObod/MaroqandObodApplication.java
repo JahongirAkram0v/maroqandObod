893eded6d18e280dc404a3e352e470584333719e
JahongirAkram0v <jahongirakramov0000@gmail.com>
2026-02-26 14:20:19 +0500

    init project

---
+++ src/main/java/uz/samtuit/maroqandObod/MaroqandObodApplication.java
@@ -0,0 +1,13 @@
+package uz.samtuit.maroqandObod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaroqandObodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaroqandObodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/samtuit/maroqandObod/MaroqandObodApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.samtuit.maroqandObod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaroqandObodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
